423fc3c4c50364495df34d0d834960fe093ff051
andreendo <andreendo@gmail.com>
2020-04-06 22:05:08 -0300

    refactoring - try 4

--- topico2/exemplo01/lib/extremos/utilitario.js
+++ topico2/exemplo01/lib/extremos/utilitario.js
@@ -1,32 +1,26 @@
 const R = require('ramda');
 
-const reduceMaior = (mAtual, currentValue, currentIndex) => {
-    if (currentValue > mAtual.maior)
-        return {
-            maior: currentValue,
-            indiceMaior: currentIndex
-        };
+const reduceMaior = (mAtual, currentValue, currentIndex) => R.when(
+    mAtual => currentValue > mAtual.maior,
+    () => ({
+        maior: currentValue,
+        indiceMaior: currentIndex
+    })
+)(mAtual);
 
-    return mAtual;
-};
+const reduceMenor = (mAtual, currentValue, currentIndex) => R.when(
+    mAtual => currentValue < mAtual.menor,
+    () => ({
+        menor: currentValue,
+        indiceMenor: currentIndex
+    })
+)(mAtual);
 
-const reduceMenor = (mAtual, currentValue, currentIndex) => {
-    if (currentValue < mAtual.menor)
-        return {
-            menor: currentValue,
-            indiceMenor: currentIndex
-        };
-
-    return mAtual;
-};
-
-exports.acharExtremos = arr => {
-    return R.ifElse(
-        arr => arr == null || arr.length === 0,
-        () => ({}),
-        (arr) => ({
-            ...arr.reduce(reduceMaior, { maior: arr[0], indiceMaior: 0 }),
-            ...arr.reduce(reduceMenor, { menor: arr[0], indiceMenor: 0 })
-        })
-    )(arr);
-}
+exports.acharExtremos = arr => R.ifElse(
+    arr => arr == null || arr.length === 0,
+    () => ({}),
+    arr => ({
+        ...arr.reduce(reduceMaior, { maior: arr[0], indiceMaior: 0 }),
+        ...arr.reduce(reduceMenor, { menor: arr[0], indiceMenor: 0 })
+    })
+)(arr);
