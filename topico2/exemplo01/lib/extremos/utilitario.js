d450fa6175550f310e5d0fee63ce57c0ab2583b3
andreendo <andreendo@gmail.com>
2020-04-06 21:38:36 -0300

    refactoring - try 3

--- topico2/exemplo01/lib/extremos/utilitario.js
+++ topico2/exemplo01/lib/extremos/utilitario.js
@@ -1,23 +1,32 @@
 const R = require('ramda');
 
 const reduceMaior = (mAtual, currentValue, currentIndex) => {
-    if (currentValue > mAtual.maior) {
-        return { maior: currentValue, indiceMaior: currentIndex };
-    }
+    if (currentValue > mAtual.maior)
+        return {
+            maior: currentValue,
+            indiceMaior: currentIndex
+        };
+
     return mAtual;
 };
 
 const reduceMenor = (mAtual, currentValue, currentIndex) => {
-    if (currentValue < mAtual.menor) {
-        return { menor: currentValue, indiceMenor: currentIndex };
-    }
+    if (currentValue < mAtual.menor)
+        return {
+            menor: currentValue,
+            indiceMenor: currentIndex
+        };
+
     return mAtual;
 };
 
-exports.acharExtremos = function (v) {
-    if (v == null || v.length === 0) return {};
-
-    const eMaior = v.reduce(reduceMaior, { maior: v[0], indiceMaior: 0 });
-    const eMenor = v.reduce(reduceMenor, { menor: v[0], indiceMenor: 0 });
-    return { ...eMaior, ...eMenor };
+exports.acharExtremos = arr => {
+    return R.ifElse(
+        arr => arr == null || arr.length === 0,
+        () => ({}),
+        (arr) => ({
+            ...arr.reduce(reduceMaior, { maior: arr[0], indiceMaior: 0 }),
+            ...arr.reduce(reduceMenor, { menor: arr[0], indiceMenor: 0 })
+        })
+    )(arr);
 }
